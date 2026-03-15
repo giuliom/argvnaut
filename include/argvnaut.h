b516871f23509733775bd0967a357b7632c7c37c
Giulio M <180579974+reeftide-giuliom@users.noreply.github.com>
2026-03-15 18:08:52 +0000

    Basic implementation

--- include/argvnaut.h
+++ include/argvnaut.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <algorithm>
+#include <optional>
 #include <string>
+#include <unordered_map>
+#include <variant>
 #include <vector>
 
 namespace ArgvNaut {
@@ -22,6 +26,7 @@ enum class OptionType {
 
 struct PositionalArg {
     std::string name;
+    size_t position = 0; // 1-indexed
     bool required = true;
 };
 
@@ -39,7 +44,7 @@ struct FlagArg {
 struct OptionArg {
     std::string name;
     std::string shortName;
-    OptionType optionType;
+    OptionType optionType = OptionType::STRING;
     std::string defaultValue;
     bool required = false;
 };
@@ -48,51 +53,248 @@ using ArgumentVariant = std::variant<PositionalArg, FlagArg, OptionArg, Subcomma
 
 struct Argument 
 {
-    const ArgumentType type;
+    ArgumentType type;
     ArgumentVariant data;
     
     template<typename T>
     const T& get() const {
         return std::get<T>(data);
     }
-
 };
+
+using ParsedValue = std::variant<std::string, bool, int, float>;
 
 class Parser 
 {
 public:
-    Parser(const std::string& programName) : m_programName(programName)
-    {
-
-    }
+    Parser(const std::string& programName) : m_programName(programName) {}
 
     bool addPositional(const std::string& name, size_t position)
     {
+        if (m_args.contains(name)) return false;
+        if (m_positionalsByPosition.contains(position)) return false;
+
+        PositionalArg pos;
+        pos.name = name;
+        pos.position = position;
+
+        m_args.emplace(name, Argument{ArgumentType::POSITIONAL, pos});
+        m_positionalsByPosition[position] = name;
         return true;
     }
 
-    bool parse(const int argc, const char** argv) const
+    bool addFlag(const std::string& name, const std::string& shortName = "")
     {
+        if (m_args.contains(name)) return false;
+
+        FlagArg flag;
+        flag.name = name;
+        flag.shortName = shortName;
+
+        m_args.emplace(name, Argument{ArgumentType::FLAG, flag});
+
+        m_longNameLookup["--" + name] = name;
+        if (!shortName.empty()) {
+            m_shortNameLookup["-" + shortName] = name;
+        }
         return true;
+    }
+
+    bool addOption(const std::string& name, const std::string& shortName = "",
+                   OptionType optionType = OptionType::STRING,
+                   const std::string& defaultValue = "", bool required = false)
+    {
+        if (m_args.contains(name)) return false;
+
+        OptionArg opt;
+        opt.name = name;
+        opt.shortName = shortName;
+        opt.optionType = optionType;
+        opt.defaultValue = defaultValue;
+        opt.required = required;
+
+        m_args.emplace(name, Argument{ArgumentType::OPTION, opt});
+
+        m_longNameLookup["--" + name] = name;
+        if (!shortName.empty()) {
+            m_shortNameLookup["-" + shortName] = name;
+        }
+        return true;
+    }
+
+    bool parse(int argc, const char** argv)
+    {
+        m_results.clear();
+
+        // Set defaults
+        for (const auto& [name, arg] : m_args) {
+            if (arg.type == ArgumentType::FLAG) {
+                m_results[name] = false;
+            } else if (arg.type == ArgumentType::OPTION) {
+                const auto& opt = arg.get<OptionArg>();
+                if (!opt.defaultValue.empty()) {
+                    m_results[name] = opt.defaultValue;
+                }
+            }
+        }
+
+        size_t positionalIndex = 1;
+
+        for (int i = 1; i < argc; ++i) {
+            std::string token(argv[i]);
+            std::optional<std::string> next;
+            if (i + 1 < argc) {
+                next = std::string(argv[i + 1]);
+            }
+
+            if (token.starts_with("--") || token.starts_with("-")) {
+                size_t consumed = parseNamedArg(token, next);
+                if (consumed == 0) return false;
+                i += static_cast<int>(consumed - 1);
+            } else {
+                auto it = m_positionalsByPosition.find(positionalIndex);
+                if (it == m_positionalsByPosition.end()) {
+                    return false; // unexpected positional
+                }
+                m_results[it->second] = token;
+                ++positionalIndex;
+            }
+        }
+
+        // Validate required arguments
+        for (const auto& [name, arg] : m_args) {
+            if (arg.type == ArgumentType::POSITIONAL) {
+                const auto& pos = arg.get<PositionalArg>();
+                if (pos.required && !m_results.contains(name)) {
+                    return false;
+                }
+            } else if (arg.type == ArgumentType::OPTION) {
+                const auto& opt = arg.get<OptionArg>();
+                if (opt.required && !m_results.contains(name)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    std::optional<std::string> getString(const std::string& name) const
+    {
+        auto it = m_results.find(name);
+        if (it == m_results.end()) return std::nullopt;
+        if (auto* s = std::get_if<std::string>(&it->second)) {
+            return *s;
+        }
+        return std::nullopt;
+    }
+
+    bool getFlag(const std::string& name) const
+    {
+        auto it = m_results.find(name);
+        if (it == m_results.end()) return false;
+        if (auto* b = std::get_if<bool>(&it->second)) {
+            return *b;
+        }
+        return false;
+    }
+
+    std::optional<int> getInt(const std::string& name) const
+    {
+        auto it = m_results.find(name);
+        if (it == m_results.end()) return std::nullopt;
+        if (auto* v = std::get_if<int>(&it->second)) {
+            return *v;
+        }
+        if (auto* s = std::get_if<std::string>(&it->second)) {
+            try { return std::stoi(*s); } catch (...) { return std::nullopt; }
+        }
+        return std::nullopt;
+    }
+
+    std::optional<float> getFloat(const std::string& name) const
+    {
+        auto it = m_results.find(name);
+        if (it == m_results.end()) return std::nullopt;
+        if (auto* v = std::get_if<float>(&it->second)) {
+            return *v;
+        }
+        if (auto* s = std::get_if<std::string>(&it->second)) {
+            try { return std::stof(*s); } catch (...) { return std::nullopt; }
+        }
+        return std::nullopt;
+    }
+
+    bool has(const std::string& name) const
+    {
+        return m_results.contains(name);
     }
 
 private:
-
-    size_t parseArgument(const std::string& arg, std::optional<std::string> next) const
+    // Returns number of tokens consumed (1 for flag, 2 for option with value), 0 on error
+    size_t parseNamedArg(const std::string& token, std::optional<std::string> next)
     {
-        return 1;
-    }
+        std::string lookupToken = token;
+        std::optional<std::string> inlineValue;
 
-    size_t parseOption(const std::string& arg, std::optional<std::string> next) const
-    {
-        return 1;
-    }
+        // Handle --name=value syntax
+        if (token.starts_with("--")) {
+            auto eqPos = token.find('=');
+            if (eqPos != std::string::npos) {
+                lookupToken = token.substr(0, eqPos);
+                inlineValue = token.substr(eqPos + 1);
+            }
+        }
 
+        // Resolve argument name from long or short form
+        std::string argName;
+        auto longIt = m_longNameLookup.find(lookupToken);
+        if (longIt != m_longNameLookup.end()) {
+            argName = longIt->second;
+        } else {
+            auto shortIt = m_shortNameLookup.find(lookupToken);
+            if (shortIt != m_shortNameLookup.end()) {
+                argName = shortIt->second;
+            } else {
+                return 0; // unknown argument
+            }
+        }
+
+        auto argIt = m_args.find(argName);
+        if (argIt == m_args.end()) return 0;
+
+        const auto& arg = argIt->second;
+
+        if (arg.type == ArgumentType::FLAG) {
+            m_results[argName] = true;
+            return 1;
+        } else if (arg.type == ArgumentType::OPTION) {
+            std::string value;
+            size_t consumed = 1;
+
+            if (inlineValue) {
+                value = *inlineValue;
+            } else if (next) {
+                value = *next;
+                consumed = 2;
+            } else {
+                return 0; // option requires a value
+            }
+
+            m_results[argName] = value;
+            return consumed;
+        }
+
+        return 0;
+    }
 
 private:
     std::string m_programName;
     std::unordered_map<std::string, Argument> m_args;
+    std::unordered_map<size_t, std::string> m_positionalsByPosition;
+    std::unordered_map<std::string, std::string> m_longNameLookup;  // "--name" -> "name"
+    std::unordered_map<std::string, std::string> m_shortNameLookup; // "-n" -> "name"
+    std::unordered_map<std::string, ParsedValue> m_results;
 };
-
 
 } // namespace ArgvNaut
--- test/argvtests.cpp
+++ test/argvtests.cpp
@@ -4,11 +4,188 @@
 
 #define SUITE_NAME TestArgvnaut
 
-TEST(SUITE_NAME, testOneArg) 
+// --- Positional arguments ---
+
+TEST(SUITE_NAME, testOnePositional) 
 {
     ArgvNaut::Parser parser("testprog");
     parser.addPositional("input", 1);
     const bool result = parser.parse(2, (const char*[]){"testprog", "file.txt"});
     EXPECT_TRUE(result);
+    EXPECT_EQ(parser.getString("input").value(), "file.txt");
+}
 
+TEST(SUITE_NAME, testTwoPositionals)
+{
+    ArgvNaut::Parser parser("testprog");
+    parser.addPositional("input", 1);
+    parser.addPositional("output", 2);
+    const bool result = parser.parse(3, (const char*[]){"testprog", "in.txt", "out.txt"});
+    EXPECT_TRUE(result);
+    EXPECT_EQ(parser.getString("input").value(), "in.txt");
+    EXPECT_EQ(parser.getString("output").value(), "out.txt");
+}
+
+TEST(SUITE_NAME, testMissingRequiredPositional)
+{
+    ArgvNaut::Parser parser("testprog");
+    parser.addPositional("input", 1);
+    const bool result = parser.parse(1, (const char*[]){"testprog"});
+    EXPECT_FALSE(result);
+}
+
+// --- Flags ---
+
+TEST(SUITE_NAME, testFlagLong)
+{
+    ArgvNaut::Parser parser("testprog");
+    parser.addFlag("verbose", "v");
+    const bool result = parser.parse(2, (const char*[]){"testprog", "--verbose"});
+    EXPECT_TRUE(result);
+    EXPECT_TRUE(parser.getFlag("verbose"));
+}
+
+TEST(SUITE_NAME, testFlagShort)
+{
+    ArgvNaut::Parser parser("testprog");
+    parser.addFlag("verbose", "v");
+    const bool result = parser.parse(2, (const char*[]){"testprog", "-v"});
+    EXPECT_TRUE(result);
+    EXPECT_TRUE(parser.getFlag("verbose"));
+}
+
+TEST(SUITE_NAME, testFlagDefault)
+{
+    ArgvNaut::Parser parser("testprog");
+    parser.addFlag("verbose", "v");
+    const bool result = parser.parse(1, (const char*[]){"testprog"});
+    EXPECT_TRUE(result);
+    EXPECT_FALSE(parser.getFlag("verbose"));
+}
+
+// --- Options ---
+
+TEST(SUITE_NAME, testOptionLong)
+{
+    ArgvNaut::Parser parser("testprog");
+    parser.addOption("output", "o");
+    const bool result = parser.parse(3, (const char*[]){"testprog", "--output", "file.txt"});
+    EXPECT_TRUE(result);
+    EXPECT_EQ(parser.getString("output").value(), "file.txt");
+}
+
+TEST(SUITE_NAME, testOptionShort)
+{
+    ArgvNaut::Parser parser("testprog");
+    parser.addOption("output", "o");
+    const bool result = parser.parse(3, (const char*[]){"testprog", "-o", "file.txt"});
+    EXPECT_TRUE(result);
+    EXPECT_EQ(parser.getString("output").value(), "file.txt");
+}
+
+TEST(SUITE_NAME, testOptionEqualssyntax)
+{
+    ArgvNaut::Parser parser("testprog");
+    parser.addOption("output", "o");
+    const bool result = parser.parse(2, (const char*[]){"testprog", "--output=file.txt"});
+    EXPECT_TRUE(result);
+    EXPECT_EQ(parser.getString("output").value(), "file.txt");
+}
+
+TEST(SUITE_NAME, testOptionDefault)
+{
+    ArgvNaut::Parser parser("testprog");
+    parser.addOption("output", "o", ArgvNaut::OptionType::STRING, "default.txt");
+    const bool result = parser.parse(1, (const char*[]){"testprog"});
+    EXPECT_TRUE(result);
+    EXPECT_EQ(parser.getString("output").value(), "default.txt");
+}
+
+TEST(SUITE_NAME, testRequiredOptionMissing)
+{
+    ArgvNaut::Parser parser("testprog");
+    parser.addOption("output", "o", ArgvNaut::OptionType::STRING, "", true);
+    const bool result = parser.parse(1, (const char*[]){"testprog"});
+    EXPECT_FALSE(result);
+}
+
+// --- Numeric options ---
+
+TEST(SUITE_NAME, testIntOption)
+{
+    ArgvNaut::Parser parser("testprog");
+    parser.addOption("count", "n", ArgvNaut::OptionType::INTEGER);
+    const bool result = parser.parse(3, (const char*[]){"testprog", "--count", "42"});
+    EXPECT_TRUE(result);
+    EXPECT_EQ(parser.getInt("count").value(), 42);
+}
+
+TEST(SUITE_NAME, testFloatOption)
+{
+    ArgvNaut::Parser parser("testprog");
+    parser.addOption("ratio", "r", ArgvNaut::OptionType::FLOAT);
+    const bool result = parser.parse(3, (const char*[]){"testprog", "--ratio", "3.14"});
+    EXPECT_TRUE(result);
+    EXPECT_NEAR(parser.getFloat("ratio").value(), 3.14f, 0.001f);
+}
+
+// --- Mixed arguments ---
+
+TEST(SUITE_NAME, testMixedArgs)
+{
+    ArgvNaut::Parser parser("testprog");
+    parser.addPositional("input", 1);
+    parser.addFlag("verbose", "v");
+    parser.addOption("output", "o");
+
+    const bool result = parser.parse(6, (const char*[]){"testprog", "-v", "--output", "out.txt", "in.txt", "--verbose"});
+    EXPECT_TRUE(result);
+    EXPECT_EQ(parser.getString("input").value(), "in.txt");
+    EXPECT_EQ(parser.getString("output").value(), "out.txt");
+    EXPECT_TRUE(parser.getFlag("verbose"));
+}
+
+// --- Error cases ---
+
+TEST(SUITE_NAME, testUnknownFlag)
+{
+    ArgvNaut::Parser parser("testprog");
+    const bool result = parser.parse(2, (const char*[]){"testprog", "--unknown"});
+    EXPECT_FALSE(result);
+}
+
+TEST(SUITE_NAME, testExtraPositional)
+{
+    ArgvNaut::Parser parser("testprog");
+    parser.addPositional("input", 1);
+    const bool result = parser.parse(3, (const char*[]){"testprog", "a.txt", "b.txt"});
+    EXPECT_FALSE(result);
+}
+
+// --- has() ---
+
+TEST(SUITE_NAME, testHas)
+{
+    ArgvNaut::Parser parser("testprog");
+    parser.addPositional("input", 1);
+    parser.addOption("output", "o");
+    parser.parse(2, (const char*[]){"testprog", "in.txt"});
+    EXPECT_TRUE(parser.has("input"));
+    EXPECT_FALSE(parser.has("output"));
+}
+
+// --- Duplicate registration ---
+
+TEST(SUITE_NAME, testDuplicatePositionalName)
+{
+    ArgvNaut::Parser parser("testprog");
+    EXPECT_TRUE(parser.addPositional("input", 1));
+    EXPECT_FALSE(parser.addPositional("input", 2));
+}
+
+TEST(SUITE_NAME, testDuplicatePositionalPosition)
+{
+    ArgvNaut::Parser parser("testprog");
+    EXPECT_TRUE(parser.addPositional("input", 1));
+    EXPECT_FALSE(parser.addPositional("output", 1));
 }
