e4f53271ded7cc33b5b5cc6f8b7f622721c40429
Giulio M <180579974+reeftide-giuliom@users.noreply.github.com>
2026-03-29 22:47:14 +0100

    Improved vs code integration

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <argvnaut.h>
+
+
+
+int main()
+{
+    ArgvNaut::Parser parser("testprog");
+    parser.addFlag("help", "h");
+	parser.addOption("width", "w", ArgvNaut::OptionType::INTEGER, "1280");
+	parser.addOption("height", "e", ArgvNaut::OptionType::INTEGER, "720");
+	parser.addOption("output", "o", ArgvNaut::OptionType::STRING, "");
+	parser.addOption("rendering", "r", ArgvNaut::OptionType::STRING, "raytracer");
+	parser.addOption("shading", "s", ArgvNaut::OptionType::STRING, "lit");
+	parser.addOption("file", "f", ArgvNaut::OptionType::STRING, "");
+	parser.addOption("samples", "p", ArgvNaut::OptionType::INTEGER, "4");
+	parser.addOption("bounces", "b", ArgvNaut::OptionType::INTEGER, "3");
+
+    parser.parse(2, (const char*[]){"testprog", "-h"});
+
+    parser.getFlag("help");
+
+    std::cout<< "Help: " << (parser.getFlag("help") ? "true" : "false") << std::endl;
+
+    return 0;
+}
